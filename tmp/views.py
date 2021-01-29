f2275561fd1f43df1c872a74098f55204a3db609
Kulvir Pratap Singh <kulvir72510@gmail.com>
2021-01-29 13:26:25 +0530

    ok

--- tmp/views.py
+++ tmp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def page1(request):
+    return render(request,"tmp/page1.html",)
+
+def trycurve(request):
+    return render(request,"tmp/trycurve.html",)
