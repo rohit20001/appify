6e99016d8e4c26fa700d6188cfa6d45fbeb6779c
Kulvir Pratap Singh <kulvir72510@gmail.com>
2021-01-30 20:27:17 +0530

    project page

--- tmp/urls.py
+++ tmp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.page2,name = "page2"),
-    path('page1/',views.page1,name = "page1"),
-    path('blog/',views.blog,name = "blog"),
+    path('', views.page1, name="page1"),
+    path('blog/', views.blog, name="blog"),
+    path('page1/', views.page1, name="page1"),
+    path('projects/', views.projects, name="projects"),
 ]
