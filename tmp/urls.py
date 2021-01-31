fd95fadd001dc61654cedaec2c85d3a845d3f6d5
rohit verma <rohit9721382965@gmail.com>
2021-01-31 14:43:31 +0530

    courses page

--- tmp/urls.py
+++ tmp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('blog/', views.blog, name="blog"),
     path('page1/', views.page1, name="page1"),
     path('projects/', views.projects, name="projects"),
+    path('page2/', views.page2, name="page2"),
+    path('courses/', views.courses, name="courses"),
 ]
--- tmp/views.py
+++ tmp/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def page1(request):
     return render(request,"tmp/page1.html",)
 
+def page2(request):
+    return render(request,"tmp/page2.html",)
+
+def courses(request):
+    return render(request,"tmp/courses.html",)
+
 def blog(request):
     context = {
         "data": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
