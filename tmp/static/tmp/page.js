b215ff2933302e98e9eed4b166eeef6672ee82d3
ghostcode-sys <prashantchandel2103@gmail.com>
2021-01-31 21:44:48 +0530

    animation added

--- tmp/static/tmp/page.js
+++ tmp/static/tmp/page.js
@@ -5,4 +5,27 @@ $('.icon').click(function(){
 $('.king').click(function(){
     $('.togle').hide();
 });
+// Animation for feedback
+    $('#scrleft').click(function(){
+        $(".sub5").fadeOut();
+         $(".dsub5").animate({
+            height:'20px',
+            width: '5%',
+            overflow:'visible',
+        });
+        $(".dsub5").animate({
+            right: "-100%"
+        });
+    });
+    $('#scrright').click(function(){
+        $(".sub5").fadeOut();
+        $(".dsub5").animate({
+            height:'20px',
+            width: '5%',
+            overflow:'visible',
+        });
+        $(".dsub5").animate({
+            left: "-100%"
+        });
+    });
 });
